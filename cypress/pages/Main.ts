8fab80f6ac241430b58330a8c9f4acd21ca39f1c
carmenjoy <67772382+cjp-engr@users.noreply.github.com>
2023-01-02 22:58:52 +0800

    home.ts and main.ts updated

--- cypress/pages/Main.ts
+++ cypress/pages/Main.ts
@@ -1,5 +1,139 @@
 class Main {
 
+    //--start navigation bar--//
+    private navigationBarHome: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1226']";
+    private navigationBarStore: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1227']";
+    private navigationBarMen: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1228']";
+    private navigationBarWomen: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1229']";
+    private navigationBarAccessories: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1230']";
+    private navigationBarAccount: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1237']";
+    private navigationBarAbout: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1232']";
+    private navigationBarContactUs: string = "ul[id='ast-hf-menu-1'] li[id='menu-item-1233']";
+    private cart: string = "div[class='site-primary-header-wrap ast-builder-grid-row-container site-header-focus-item ast-container'] a[title='View your shopping cart']";
+    private headerTitle: string = "h1[itemprop='name'] a[href='https://askomdch.com/']";
+    //--end navigation bar--//
+
+    //--start footer--//
+    private quickLinkHome: string = "ul[id='menu-quick-links'] li[id='menu-item-1254']";
+    private quickLinkAbout: string = "ul[id='menu-quick-links'] li[id='menu-item-1255']";
+    private quickLinkMyAccount: string = "ul[id='menu-quick-links'] li[id='menu-item-1256']";
+    private quickLinkCart: string = "ul[id='menu-quick-links'] li[id='menu-item-1257']";
+    private quickLinkContactUs: string = "ul[id='menu-quick-links'] li[id='menu-item-1258']";
+
+    private menuForHerWomen: string = "ul[id='menu-for-her'] li[id='menu-item-1264']";
+    private menuForHerWomensJeans: string = "ul[id='menu-for-her'] li[id='menu-item-1266']";
+    private menuForHerWomensShirts: string = "ul[id='menu-for-her'] li[id='menu-item-1265']";
+    private menuForHerWomensShoes: string = "ul[id='menu-for-her'] li[id='menu-item-1267']";
+    private menuForHerAccessories: string = "ul[id='menu-for-her'] li[id='menu-item-1268']";
+
+    private menuForHimMen: string = "ul[id='menu-for-him'] li[id='menu-item-1263']";
+    private menuForHimMensJeans: string = "ul[id='menu-for-him'] li[id='menu-item-1259']";
+    private menuForHimMensShirts: string = "ul[id='menu-for-him'] li[id='menu-item-1260']";
+    private menuForHimMensShoes: string = "ul[id='menu-for-him'] li[id='menu-item-1261']";
+    private menuForHimAccessories: string = "ul[id='menu-for-him'] li[id='menu-item-1262']";
+
+    //--end footer--//
+
+    get navigationBarHomeElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarHome);
+    }
+
+    get navigationBarStoreElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarStore);
+    }
+
+    get navigationBarMenElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarMen);
+    }
+
+    get navigationBarWomenElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarWomen);
+    }
+
+    get navigationBarAccessoriesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarAccessories);
+    }
+
+    get navigationBarAccountElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarAccount);
+    }
+
+    get navigationBarAboutElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarAbout);
+    }
+
+    get navigationBarContactUsElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.navigationBarContactUs);
+    }
+
+    get cartElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.cart);
+    }
+
+    get headerTitleElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.headerTitle);
+    }
+
+    get quickLinkHomeElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.quickLinkHome);
+    }
+
+    get quickLinkAboutElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.quickLinkAbout);
+    }
+
+    get quickLinkMyAccountElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.quickLinkMyAccount);
+    }
+
+    get quickLinkCartElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.quickLinkCart);
+    }
+
+    get quickLinkContactUsElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.quickLinkContactUs);
+    }
+
+    get menuForHerWomenElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHerWomen);
+    }
+
+    get menuForHerWomensJeansElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHerWomensJeans);
+    }
+
+    get menuForHerWomensShirtsElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHerWomensShirts);
+    }
+
+    get menuForHerWomensShoesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHerWomensShoes);
+    }
+
+    get menuForHerAccessoriesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHerAccessories);
+    }
+
+    get menuForHimMenElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHimMen);
+    }
+
+    get menuForHimMensJeansElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHimMensJeans);
+    }
+
+    get menuForHimMensShirtsElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHimMensShirts);
+    }
+
+    get menuForHimMensShoesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHimMensShoes);
+    }
+
+    get menuForHimAccessoriesElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.menuForHimAccessories);
+    }
+
 }
 
 export const MainClass = new Main();
--- cypress/pages/home/Home.ts
+++ cypress/pages/home/Home.ts
@@ -1,5 +1,62 @@
 class Home {
 
+    //--start header--//
+    private headerTitle: string = ".alignwide.has-text-align-center.has-white-color.has-text-color.has-huge-font-size";
+    private headerSubtitle: string = ".has-text-align-center.has-white-color.has-text-color.has-medium-font-size";
+    private headerShopNowButton: string = ".wp-block-button__link[href='/store']";
+    private headerFindMoreButton: string = ".wp-block-button__link[href*='/contact-us']";
+    //--end header--//
+
+    //--start body featured categories--//
+    private featuredCategoriesWomenButton: string = ".wp-block-button__link[href*='/women']";
+    private featuredCategoriesMenButton: string = ".wp-block-button__link[href*='/men']";
+    private featuredCategoriesAccessoriesButton: string = ".wp-block-button__link[href*='/accessories']";
+    private featuredCategoriesTitle: string;
+    private featuredCategoriesSubtitle: string;
+    //--end body featured categories--//
+
+    //--start body featured products--//
+    private featuredProductsText: string = "h2[class='has-text-align-center']";
+    private featuredProductsImage: string;
+    private featuredProductsSaleThumbnail: string;
+    private featuredProductsName: string;
+    private featuredProductsCategory: string;
+    private featuredProductsPrice: string;
+    private featuredProductsRating: string;
+    private featuredProductsAddToCart: string;
+    //--end body featured products--//
+
+    get headerTitleElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.headerTitle);
+    }
+
+    get headerSubtitleElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.headerSubtitle);
+    }
+
+    get headerShopNowButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.headerShopNowButton);
+    }
+
+    get headerFindMoreButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.headerFindMoreButton);
+    }
+
+    get featuredCategoriesWomenButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.featuredCategoriesWomenButton);
+    }
+
+    get featuredCategoriesMenButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.featuredCategoriesMenButton);
+    }
+
+    get featuredCategoriesAccessoriesButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.featuredCategoriesAccessoriesButton);
+    }
+
+    get featuredProductsTextElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.featuredProductsText);
+    }
 }
 
 export const HomePage = new Home();
