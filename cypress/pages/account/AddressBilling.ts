c4fcc8f27a85e1cc09588f04de86c4b72248b72d
carmenjoy <67772382+cjp-engr@users.noreply.github.com>
2023-01-07 12:22:14 +0800

    updated test script

--- cypress/pages/account/AddressBilling.ts
+++ cypress/pages/account/AddressBilling.ts
@@ -1,4 +1,4 @@
-class Addresses {
+class AddressBilling {
     private addressesNavigationLink: string = ".woocommerce-MyAccount-navigation-link--edit-address";
     private billingAddressText: string = "";
     private billingAddButton: string = "a[href*='/edit-address/billing']";
@@ -14,19 +14,6 @@ class Addresses {
     private billingPhoneTextField: string = "#billing_phone";
     private billingEmailAddressTextField: string = "#billing_email";
     private billingSaveAddressButton: string = "button[name='save_address']";
-
-    private shippingAddressText: string = "";
-    private shippingAddButton: string = "a[href*='/edit-address/shipping']";
-    private shippingFirstNameTextField: string = "#shipping_first_name";
-    private shippingLastNameTextField: string = "#shipping_last_name";
-    private shippingCompanyNameTextField: string = "#shipping_company";
-    private shippingCountryRegionSelectField: string = "select[name='shipping_country']";
-    private shippingStreetAddressFirstTextField: string = "#shipping_address_1";
-    private shippingStreetAddressSecondTextField: string = "#shipping_address_2";
-    private shippingTownCityTextField: string = "#shipping_city";
-    private shippingStateSelectField: string = "#shipping_state";
-    private shippingZipCodeTextField: string = "#shipping_postcode";
-    private shippingSaveAddressButton: string = "button[name='save_address']";
 
     get addressesNavigationLinkElement(): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.get(this.addressesNavigationLink);
@@ -84,52 +71,6 @@ class Addresses {
     get billingSaveAddressButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.get(this.billingSaveAddressButton);
     }
-
-    get shippingAddressTextElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingAddressText);
-    }
-
-    get shippingAddButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingAddButton);
-    }
-    get shippingFirstNameTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingFirstNameTextField);
-    }
-
-    get shippingLastNameTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingLastNameTextField);
-    }
-
-    get shippingCompanyNameTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingCompanyNameTextField);
-    }
-
-    get shippingCountryRegionSelectFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingCountryRegionSelectField);
-    }
-
-    get shippingStreetAddressFirstTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingStreetAddressFirstTextField);
-    }
-    get shippingStreetAddressSecondTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingStreetAddressSecondTextField);
-    }
-
-    get shippingTownCityTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingTownCityTextField);
-    }
-
-    get shippingStateSelectFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingStateSelectField);
-    }
-
-    get shippingZipCodeTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingZipCodeTextField);
-    }
-
-    get shippingSaveAddressButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
-        return cy.get(this.shippingSaveAddressButton);
-    }
 }
 
-export const AddressesPage = new Addresses();
+export const AddressBillingPage = new AddressBilling();
---
+++ cypress/pages/account/AddressShipping.ts
@@ -0,0 +1,67 @@
+class AddressShipping {
+    private addressesNavigationLink: string = ".woocommerce-MyAccount-navigation-link--edit-address";
+    private shippingAddressText: string = "";
+    private shippingAddButton: string = "a[href*='/edit-address/shipping']";
+    private shippingFirstNameTextField: string = "#shipping_first_name";
+    private shippingLastNameTextField: string = "#shipping_last_name";
+    private shippingCompanyNameTextField: string = "#shipping_company";
+    private shippingCountryRegionSelectField: string = "select[name='shipping_country']";
+    private shippingStreetAddressFirstTextField: string = "#shipping_address_1";
+    private shippingStreetAddressSecondTextField: string = "#shipping_address_2";
+    private shippingTownCityTextField: string = "#shipping_city";
+    private shippingStateSelectField: string = "#shipping_state";
+    private shippingZipCodeTextField: string = "#shipping_postcode";
+    private shippingSaveAddressButton: string = "button[name='save_address']";
+
+    get addressesNavigationLinkElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.addressesNavigationLink);
+    }
+
+    get shippingAddressTextElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingAddressText);
+    }
+
+    get shippingAddButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingAddButton);
+    }
+    get shippingFirstNameTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingFirstNameTextField);
+    }
+
+    get shippingLastNameTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingLastNameTextField);
+    }
+
+    get shippingCompanyNameTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingCompanyNameTextField);
+    }
+
+    get shippingCountryRegionSelectFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingCountryRegionSelectField);
+    }
+
+    get shippingStreetAddressFirstTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingStreetAddressFirstTextField);
+    }
+    get shippingStreetAddressSecondTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingStreetAddressSecondTextField);
+    }
+
+    get shippingTownCityTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingTownCityTextField);
+    }
+
+    get shippingStateSelectFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingStateSelectField);
+    }
+
+    get shippingZipCodeTextFieldElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingZipCodeTextField);
+    }
+
+    get shippingSaveAddressButtonElement(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(this.shippingSaveAddressButton);
+    }
+}
+
+export const AddressShippingPage = new AddressShipping();
