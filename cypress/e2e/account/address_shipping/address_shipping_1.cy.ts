f8b48e3a290368f8928e26d0dcb9e5b7adc270d4
carmenjoy <67772382+cjp-engr@users.noreply.github.com>
2023-01-10 15:53:13 +0800

    updated test script

--- cypress/e2e/account/address_shipping/address_shipping_1.cy.ts
+++ cypress/e2e/account/address_shipping/address_shipping_1.cy.ts
@@ -26,43 +26,146 @@ describe('Account Shipping Address Page', () => {
 
                 cy.url().should('contain', data.links['edit-address']);
                 AddressShippingPage.successMessageTextElement.should('contain.text', data.successMessage);
+
+                //todo
+                //assertion for 
             });
         });
 
         it('2. The information should not be saved if the "first name" is empty and the other required fields are filled', () => {
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                // AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
 
         });
 
         it('3. The information should not be saved if the "last name" is empty and the other required fields are filled', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                // AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('4. The information should not be saved if the "country/region" is empty and the other required fields are filled', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                // AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                // AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('5. The information should not be saved if the "street address first field" is empty and the other required fields are filled', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                // AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('6. The information should not be saved if the "town/city" is empty and the other required fields are filled', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                // AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('7. The information should not be saved if the "state" is empty and the other required fields are filled', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                // AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('8. The information should not be saved if the "zip" is empty and the other required fields are filled', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                // AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('9. The information should not be saved if all the required fields are empty', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
 
         it('10. The information should be saved succesfully after the user entered data to all the required fields but all the optional fields are empty', () => {
-
+            AddressShippingPage.clearAllFields();
+            cy.get<AccountAddressShippingTD>('@shipping').then((data) => {
+                AddressShippingPage.firstNameTextFieldElement.type(data.user['firstName']);
+                AddressShippingPage.lastNameTextFieldElement.type(data.user['lastName']);
+                // AddressShippingPage.companyNameTextFieldElement.type(data.user['companyName']);
+                AddressShippingPage.countryRegionSelectFieldElement.select(data.user['countryRegion']);
+                AddressShippingPage.streetAddressFirstTextFieldElement.type(data.user['streetAddressFirst']);
+                // AddressShippingPage.streetAddressSecondTextFieldElement.type(data.user['streetAddressSecond']);
+                AddressShippingPage.townCityTextFieldElement.type(data.user['townCity']);
+                AddressShippingPage.stateSelectFieldElement.select(data.user['state']);
+                AddressShippingPage.zipCodeTextFieldElement.type(data.user['zipCode']);
+                AddressShippingPage.saveAddressButtonElement.click();
+            });
         });
     });
 });
